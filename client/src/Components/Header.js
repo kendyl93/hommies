cfa031ecfcf7e7657e67aaf3249741eaa1d1b49f
Pawel Stanecki <pawel.stanecki@sandstream.pl>
2019-02-02 17:55:54 +0100

    Materialize and dev keys updated

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -2,7 +2,20 @@ import React, { Component } from 'react';
 
 class Header extends Component {
   render() {
-    return <div>HEADER</div>;
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <a href="#" className="left brand-logo">
+            Hommies
+          </a>
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <li>
+              <a href="sass.html">Login</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
   }
 }
 
